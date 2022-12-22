f6f71fd07d01b7d5bdb35b1fe0a44ee6c24e58e0
airatmeister <zadrot.tat.99@gmail.com>
2022-12-22 18:15:21 +0300

    Added TextFormatter

--- app/src/main/java/com/textformatter/example/MainActivity.kt
+++ app/src/main/java/com/textformatter/example/MainActivity.kt
@@ -2,10 +2,40 @@ package com.textformatter.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import ru.touchin.roboswag.textprocessing.TextFormatter
 
 class MainActivity : AppCompatActivity() {
+
+    private val textFormatter = TextFormatter(
+        regex = "(\\d{2})\\/?(\\d{2})"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText1 = findViewById<EditText>(R.id.editText1)
+
+        val buttonEditTextOne = findViewById<Button>(R.id.buttonEditTextOne)
+
+        val textViewInfoOne = findViewById<TextView>(R.id.textViewInfoOne)
+        textViewInfoOne.text = textFormatter.getRegexReplace()
+
+        val textViewInfoTwo = findViewById<TextView>(R.id.textViewInfoTwo)
+
+        buttonEditTextOne.setOnClickListener {
+            val inputText = editText1.text.toString()
+            if (inputText.isNotEmpty()) {
+                textViewInfoTwo.text = textFormatter.getFormattedText(inputText)
+            }
+        }
+
+        val editText2 = findViewById<EditText>(R.id.editText2)
+        editText2.hint = textFormatter.getPlaceholder()
+
+        textFormatter.mask(editText2)
     }
 }
